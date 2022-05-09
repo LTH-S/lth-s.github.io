8c3306b15a56c521e1cc0d55617f0f0b419da1bd
LTH-S <1306910276@qq.com>
2022-05-10 00:59:18 +0800

    Site updated: 2022-05-10 00:59:16

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -1,11 +1,10 @@
 /* global Fluid, CONFIG */
 
-function lazyload(window, document) {
+(function(window, document) {
   for (const each of document.querySelectorAll('img[lazyload]')) {
     Fluid.utils.waitElementVisible(each, function() {
       each.removeAttribute('srcset');
       each.removeAttribute('lazyload');
     }, CONFIG.lazyload.offset_factor);
   }
-}
-lazyload(window, document);
+})(window, document);
